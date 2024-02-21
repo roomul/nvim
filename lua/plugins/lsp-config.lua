17433c3415bf57020ff299e9e06fb3a3e668f565
roomul <mr.roomul@gmail.com>
2024-02-21 12:54:10 +0000

    Initial

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+        automatic_installation = true,
+        handlers = nil,
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilites = require("cmp_nvim_lsp").default_capabilites
+
+      lspconfig.lua_ls.setup({
+        capabilites = capabilites
+      })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "c", "lua", "vim"
+      },
+      auto_install = true,
+      sync_install = false,
+      highlight = {
+        enable = true
+      },
+      indent = {
+        enable = true
+      },
+    })
+  end
+}
